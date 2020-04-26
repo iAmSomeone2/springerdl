a357f56b3ce7629f3a7c4d33508c3f1ab8894383
Brenden Davidson <davidson.brenden15@gmail.com>
2020-04-25 22:52:45 -0500

    SD-3: Program now capable of intelligently locating ISBN number in any link that contains one.

--- src/bookdl.c
+++ src/bookdl.c
@@ -30,7 +30,7 @@ void init_bookdl_array(__uint32_t array_len, bookdl_t* bookdl_array, char* out_d
     for (__uint32_t i = 0; i < array_len; i++) {
         bookdl_array[i].dl_url = NULL;
         bookdl_array[i].original_url = NULL;
-        bookdl_array[i].isbn[0] = '\0';
+        bookdl_array[i].isbn = NULL;
         bookdl_array[i].out_dir = out_dir;
     }
 }
@@ -43,11 +43,32 @@ void free_bookdl_array(__uint32_t array_len, bookdl_t* bookdl_array) {
         if (bookdl_array[i].original_url != NULL) {
             free(bookdl_array[i].original_url);
         }
+        if (bookdl_array[i].isbn != NULL) {
+            free(bookdl_array[i].isbn);
+        }
     }
 }
 
 void set_isbn_number(bookdl_t* bookdl_item) {
-    strncpy(bookdl_item->isbn, bookdl_item->original_url + 50, ISBN_LEN);
+    char search_text[] = "isbn=";
+
+    __uint32_t url_len = strlen(bookdl_item->original_url);
+
+    __uint32_t read_idx = 0;
+    __uint8_t isbn_found = 0;
+    while (read_idx < (url_len - ISBN_LEN) && !isbn_found) {
+        int cmp = strncmp(bookdl_item->original_url + read_idx, search_text, 5);
+        if (cmp == 0) {
+            read_idx += 4;
+            isbn_found = 1;
+        }
+        read_idx++;
+    }
+
+    if (isbn_found) {
+        bookdl_item->isbn = (char*)(calloc(ISBN_LEN+1, sizeof(char)));
+        strncpy(bookdl_item->isbn, bookdl_item->original_url + read_idx, ISBN_LEN);
+    }
 }
 
 void set_all_isbn_numbers(__uint32_t array_len, bookdl_t* bookdl_array) {
@@ -59,10 +80,10 @@ void set_all_isbn_numbers(__uint32_t array_len, bookdl_t* bookdl_array) {
 void generate_dl_url(bookdl_t* bookdl_item) {
     size_t target_url_size = BASE_URL_LEN + ISBN_LEN + 5;
 
-    bookdl_item->dl_url = (char*)(malloc(sizeof(char) * target_url_size));
+    bookdl_item->dl_url = (char*)(calloc(target_url_size, sizeof(char)));
     strncpy(bookdl_item->dl_url, BASE_DL_URL, BASE_URL_LEN);
     strncat(bookdl_item->dl_url, bookdl_item->isbn, ISBN_LEN);
-    strncat(bookdl_item->dl_url, ".pdf", 5);
+    strncat(bookdl_item->dl_url, ".pdf", 4);
 }
 
 void generate_all_dl_urls(__uint32_t array_len, bookdl_t* bookdl_array) {
@@ -88,7 +109,7 @@ void *download_book(void *raw_bookdl_item) {
 
     // Generate filename based on book isbn
     __uint32_t dir_str_len = strlen(bookdl_item->out_dir);
-    char* out_file_name = (char*)(malloc(sizeof(char) * (dir_str_len + ISBN_LEN + 6)));
+    char* out_file_name = (char*)(calloc(dir_str_len + ISBN_LEN + 6, sizeof(char)));
     out_file_name[0] = '\0';
     strncpy(out_file_name, bookdl_item->out_dir, dir_str_len + 1);
     strncat(out_file_name, "/", 2);
